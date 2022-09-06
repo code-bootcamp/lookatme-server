49c7cf6afafe9fc8ed3c5b053c0ef85144c74345
조향래 <johyanglae@johyanglaeui-MacBookAir.local>
2022-09-06 22:22:21 +0900

    :sparkles: add and modify Story CRUD APIs

--- src/apis/stories/story.module.ts
+++ src/apis/stories/story.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from '../categories/entities/category.entity';
 import { StoryImage } from '../storyImages/entities/storyImage.entity';
+import { User } from '../users/entities/user.entity';
 import { Story } from './entities/story.entity';
 import { StoryResolver } from './story.resolver';
 import { StoryService } from './story.service';
@@ -12,6 +13,7 @@ import { StoryService } from './story.service';
       Story, //
       StoryImage,
       Category,
+      User,
     ]),
   ],
   providers: [
--- src/apis/stories/story.resolver.ts
+++ src/apis/stories/story.resolver.ts
@@ -1,5 +1,12 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { timeStamp } from 'console';
+import {
+  GqlAuthAccessGuard,
+  GqlAuthAdminAccessGuard,
+} from 'src/commons/auth/gql-auth.guard';
 import { CreateStoryInput } from './dto/createStory.input';
+import { UpdateStoryInput } from './dto/updateStory.input';
 import { Story } from './entities/story.entity';
 import { StoryService } from './story.service';
 
@@ -9,10 +16,47 @@ export class StoryResolver {
     private readonly storyService: StoryService, //
   ) {}
 
-  @Mutation(() => Story)
+  @Query(() => [Story], { description: '사연 전체 목록 조회' })
+  async fetchStories() {
+    return this.storyService.find();
+  }
+
+  @UseGuards(GqlAuthAccessGuard)
+  @Mutation(() => Story, { description: '사연 등록' })
   async createStory(
-    @Args('createStoryInput') createStoryInput: CreateStoryInput, //
+    @Context() context: any, //
+    @Args('createStoryInput') createStoryInput: CreateStoryInput,
   ) {
-    return await this.storyService.create({ createStoryInput });
+    const userId = context.req.user.id;
+    return await this.storyService.create({ createStoryInput, userId });
+  }
+
+  @UseGuards(GqlAuthAccessGuard)
+  @Mutation(() => Story, { description: '사연 수정' })
+  async updateStory(
+    @Context() context: any,
+    @Args('id') id: string, //
+    @Args('updateStoryInput') updateStoryInput: UpdateStoryInput,
+  ) {
+    const userId = context.req.user.id;
+    return await this.storyService.update({ id, userId, updateStoryInput });
+  }
+
+  @UseGuards(GqlAuthAccessGuard)
+  @Mutation(() => Boolean, { description: '자신이 등록한 사연 삭제' })
+  deleteOwnStory(
+    @Context() context: any, //
+    @Args('id') id: string,
+  ) {
+    const userId = context.req.user.id;
+    return this.storyService.deleteOwn({ id, userId });
+  }
+
+  @UseGuards(GqlAuthAdminAccessGuard)
+  @Mutation(() => Boolean, { description: '신고 사연 삭제' })
+  deleteReportedStory(
+    @Args('id') id: string, //
+  ) {
+    return this.storyService.deleteReported({ id });
   }
 }
