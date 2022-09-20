d4aae325537d4c4c57755a6ad969e954011b693f
x0ng120x <jaysong0120@gmail.com>
2022-09-20 17:44:25 +0900

    :bug: fix: deleteUser deleteLoginUSer

--- src/apis/payment/payments.module.ts
+++ src/apis/payment/payments.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comment } from '../comment/entities/comment.entity';
 import { IamportsService } from '../iamport/iamports.service';
+import { Story } from '../story/entities/story.entity';
+import { StoryImage } from '../storyImage/entities/storyImage.entity';
+import { Ticket } from '../ticket/entities/ticket.entity';
+import { UnderComment } from '../underComment/entity/underComment.entity';
+import { UnderSpecialistComment } from '../underSpecialistComment/entities/underSpecialistComment.entity';
 import { User } from '../user/entities/user.entity';
 import { UsersService } from '../user/users.service';
 import { Payment } from './entities/payment.entity';
@@ -12,6 +18,12 @@ import { PaymentsService } from './payments.service';
     TypeOrmModule.forFeature([
       Payment, //
       User,
+      Story,
+      Ticket,
+      UnderSpecialistComment,
+      Comment,
+      UnderComment,
+      StoryImage,
     ]),
   ],
   providers: [
